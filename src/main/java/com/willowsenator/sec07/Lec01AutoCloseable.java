86be848d649f3cc7bdee57ed5b1ba6ca12e26b0c
willowsenator <willowsenator@gmail.com>
2025-10-25 02:25:17 +0200

    AutoCloseable and executor types

---
+++ src/main/java/com/willowsenator/sec07/Lec01AutoCloseable.java
@@ -0,0 +1,33 @@
+package com.willowsenator.sec07;
+
+import com.willowsenator.util.CommonUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.concurrent.Executors;
+
+public class Lec01AutoCloseable {
+    private static final Logger logger = LoggerFactory.getLogger(Lec01AutoCloseable.class);
+
+    public static void main(String[] args) {
+        /*var executor = Executors.newSingleThreadExecutor();
+        executor.submit(() ->{
+            task();
+            logger.info("Submitted task");
+        });
+        executor.shutdown();*/
+
+        try (var executor = Executors.newSingleThreadExecutor()) {
+            executor.submit(() -> {
+                task();
+                logger.info("Submitted task");
+            });
+        }
+    }
+
+    private static void task() {
+        CommonUtils.sleep(Duration.ofSeconds(1));
+        logger.info("Task done");
+    }
+}
---
+++ src/main/java/com/willowsenator/sec07/Lec02ExecutorServiceTypes.java
@@ -0,0 +1,48 @@
+package com.willowsenator.sec07;
+
+import com.willowsenator.util.CommonUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class Lec02ExecutorServiceTypes {
+    private static final Logger logger = LoggerFactory.getLogger(Lec02ExecutorServiceTypes.class);
+
+    public static void main(String[] args) {
+        // execute(Executors.newSingleThreadExecutor(), 3);
+        //execute(Executors.newFixedThreadPool(5), 20);
+        //execute(Executors.newCachedThreadPool(), 200);
+        //execute(Executors.newVirtualThreadPerTaskExecutor(), 10_000);
+        scheduled();
+    }
+
+    // Schedules a task to run at a fixed rate
+    private static void scheduled() {
+        try (var executorService = Executors.newSingleThreadScheduledExecutor()) {
+            executorService.scheduleAtFixedRate(
+                    () -> logger.info("Executing task..."), 0, 1, TimeUnit.SECONDS
+            );
+            CommonUtils.sleep(Duration.ofSeconds(5));
+        }
+    }
+
+    private static void execute(ExecutorService executorService, int taskCount) {
+        try (executorService) {
+            for (int i = 0; i < taskCount; i++) {
+                int finalI = i;
+                executorService.submit(() -> ioTask(finalI));
+            }
+        }
+
+    }
+
+    private static void ioTask(int i) {
+        logger.info("Task started: {}, Thread info {}", i, Thread.currentThread().getName());
+        CommonUtils.sleep(Duration.ofSeconds(5));
+        logger.info("Task completed: {}, Thread info {}", i, Thread.currentThread().getName());
+    }
+}
