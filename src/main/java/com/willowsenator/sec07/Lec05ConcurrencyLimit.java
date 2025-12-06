a608c600e0eef62172557235622c77397f26798d
willowsenator <willowsenator@gmail.com>
2025-12-06 17:25:31 +0100

    concurrent limit with virtual threads

---
+++ src/main/java/com/willowsenator/sec07/Lec05ConcurrencyLimit.java
@@ -0,0 +1,33 @@
+package com.willowsenator.sec07;
+
+import com.willowsenator.sec07.externalservice.Client;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Lec05ConcurrencyLimit {
+
+    public static void main(String[] args) {
+        //execute(Executors.newCachedThreadPool(), 20);
+        var factory = Thread.ofVirtual().name("virtual", 1).factory();
+        execute(Executors.newFixedThreadPool(5, factory), 20);
+    }
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Lec05ConcurrencyLimit.class);
+
+    private static void execute(ExecutorService executorService, int taskCount) {
+        try (executorService) {
+            for (int i = 1; i < taskCount; i++) {
+                int finalI = i;
+                executorService.submit(() -> printProduct(finalI));
+            }
+        }
+
+    }
+
+    private static void printProduct(int id) {
+        LOGGER.info("{} => {}", id, Client.getProduct(id));
+    }
+}
